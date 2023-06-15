d376f7888f2eab2d82bd7e502a376c4724a3fafa
Jamickin <93577753+Jamickin@users.noreply.github.com>
2023-06-15 11:23:55 +0200

    James Magic
    James introduces the Vue-JavaScript-way of doing things...

--- LunarScents/src/views/HomeView.vue
+++ LunarScents/src/views/HomeView.vue
@@ -1,7 +1,19 @@
 <script setup>
-import Homebody from "../components/Homebody.vue"
 </script>
 <template>
-    <Homebody />
+    <div class="grid h-screen justify-items-center place-items-center"><img src="../assets/images/Logo.png" class="h-full">
+    </div>
+    <div class="p-4 text-center rounded-2xl shadow-2xl">
+        <h1 class="text-3xl">My name is Andrea Weichelt!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos debitis voluptatum maiores, quod mollitia placeat
+            porro itaque? Animi laboriosam eius consequatur est molestiae. Iusto, fugit. Corrupti commodi illo error <dolor
+                class="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum laborum saepe natus voluptate
+                unde debitis ullam eaque quae impedit quaerat consectetur, quasi molestiae, nulla ipsum obcaecati. Odit
+                repellendus iure <hic class="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel iure enim
+                    ratione molestias saepe fugiat modi perspiciatis suscipit consectetur corrupti quisquam sint, illum
+                    officiis inventore error nostrum cupiditate aliquam sequi.</hic>
+            </dolor>
+        </p>
+    </div>
 </template>
 <style scoped></style>
