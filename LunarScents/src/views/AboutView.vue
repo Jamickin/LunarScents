7b64a75af8d510238a52c5230c9ef031adcd9c8b
Jamickin <93577753+Jamickin@users.noreply.github.com>
2023-06-21 09:22:02 +0200

    tweaks

--- LunarScents/src/views/AboutView.vue
+++ LunarScents/src/views/AboutView.vue
@@ -1,45 +1,51 @@
-<script setup></script>
 <template>
-    <div class="flex py-12 flex-col place-items-center justify-center p-4 text-center rounded-2xl shadow-2xl">
-        <p>Greetings, fragrance enthusiasts! Welcome to the
-            extraordinary realm
-            of Lunar Scents, where scent becomes a celestial symphony and fragrances are crafted with love and cosmic
-            inspiration. I'm Andrea Weichelt, the visionary behind this ethereal fragrant journey, and I couldn't be more
-            thrilled to share it with you!
+    <!-- <div class="p-4 text-center rounded-2xl shadow-2xl">
+        <h1 class="text-3xl">Welcome to Lunar Scents</h1>
+        <br><br>
+        <p class="text-xl">
+            Greetings, fragrance enthusiasts! Welcome to the extraordinary realm of Lunar Scents, where scent becomes a
+            celestial symphony and fragrances are crafted with love and cosmic inspiration.
+
+            <br><br>
+
+            I'm Andrea Weichelt, the visionary behind this fragrant journey, and I'm thrilled to share it with you!
+
+            <br><br>
 
             At Lunar Scents, we believe that fragrances have the power to transport us to magical realms and evoke emotions
-            that
-            transcend time and space. With every scent we create, we capture the essence of celestial bodies, infusing them
-            into
-            captivating fragrances that will ignite your senses and awaken your spirit.
+            that transcend time and space. With every scent we create, we capture the essence of celestial bodies, infusing
+            them into captivating fragrances that will ignite your senses and awaken your spirit.
+
+            <br><br>
 
             Join us on this celestial odyssey as we explore a vast collection of otherworldly fragrances. Each bottle is a
             testament to our dedication and passion for the craft, meticulously crafted to transport you to distant realms
-            and
-            evoke a sense of wonder and enchantment. From the delicate blooms infused with stardust to the embrace of
-            heavenly
-            musks, our fragrances are celestial works of art that will leave you breathless.
+            and evoke a sense of wonder and enchantment. From the delicate blooms infused with stardust to the embrace of
+            heavenly musks, our fragrances are works of art that will leave you breathless.
+
+            <br><br>
 
             But Lunar Scents is more than just fragrances; it's an experience like no other. Step into our celestial-themed
-            boutiques, where twinkling lights and celestial decorations set the stage for an immersive journey. As you
-            enter,
-            you'll be enveloped in an ethereal ambiance, the air filled with the scent of wonder and the celestial melodies
+            boutiques, where ambient lighting and celestial decorations set the stage for an immersive journey. As you
+            enter, you'll be enveloped in an ethereal ambiance, the air filled with the scent of wonder and the melodies
             guiding your exploration.
 
-            I, along with my team of cosmic alchemists, invite you to indulge in the divine pleasures of scent. Allow our
-            fragrances to unlock your inner celestial being, transforming ordinary moments into extraordinary memories.
-            Embrace
-            the magic that lies within each bottle and let your senses dance among the stars.
+            <br><br>
 
-            So, my fellow fragrance enthusiasts, prepare to be enchanted by Lunar Scents. Together, let us embark on a
-            fragrant
-            voyage that transcends the mundane and takes us to the cosmos. Surrender to the allure of our celestial elixirs
-            and
-            let them guide you on an aromatic journey of infinite possibilities. Welcome to our scented universe, where
-            dreams
-            and fragrances collide in perfect harmony.</p>
-    </div>
-    <div class="grid h-screen justify-items-center place-items-center"><img src="../assets/images/Logo.png" class="h-full">
-    </div>
+            I, along with my team, invite you to indulge in the pleasures of scent. Allow our fragrances to unlock your
+            inner senses, transforming ordinary moments into extraordinary memories. Embrace the magic that lies within each
+            bottle and let your senses dance among the stars.
+
+            <br><br>
+
+            So, my fellow fragrance enthusiasts, prepare to be captivated by Lunar Scents. Together, let us embark on a
+            fragrant voyage that transcends the mundane and takes us to new horizons. Surrender to the allure of our
+            celestial creations and let them guide you on an aromatic journey of infinite possibilities. Welcome to our
+            scented universe, where dreams and fragrances collide in perfect harmony.
+        </p>
+    </div> -->
 </template>
-<style scoped></style>
+
+<style scoped>
+/* Add your custom styles for the component */
+</style>
