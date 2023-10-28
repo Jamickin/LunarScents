da81562e9fb4cf5a6a44dfa8584fa0e04a469de3
Jamickin <jamickina@gmail.com>
2023-10-28 15:41:24 +0200

    Update Product.vue

--- LunarScents/src/components/Product.vue
+++ LunarScents/src/components/Product.vue
@@ -2,7 +2,7 @@
   <div
       class="bg-neutral-50 rounded-2xl shadow-md pb-6 h-auto  transition-all duration-100 hover:scale-105">
     <img
-        :src="`../src/assets/images/${img}`" :alt="name"
+        :src="`../src/assets/images/All.webp`" :alt="name"
         class="mx-auto h-50 mb-4 rounded-t-2xl"/>
     <div class="px-2">
       <h2 class="text-2xl font-bolder text-black">
