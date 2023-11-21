df9012336e17d01223b57a381c71e87797286b56
Jamickin <jamickina@gmail.com>
2023-11-21 11:23:12 +0200

    Scroll Top

--- LunarScents/src/App.vue
+++ LunarScents/src/App.vue
@@ -1,17 +1,39 @@
 <script setup>
-import {RouterView} from "vue-router";
+import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
 import Footer from "./components/Footer.vue";
 import Stars from "./components/Stars.vue";
 </script>
 
 <template>
-  <NavBar/>
-  <main style="min-height: 90dvh;"
+  <div>
+    <NavBar />
+    <main
+      style="min-height: 90dvh;"
       class="bg-gradient-to-b from-white to-Secondary sm:mt-12"
-  >
-    <RouterView/>
-    <Stars/>
-  </main>
-  <Footer/>
+    >
+      <RouterView />
+      <Stars />
+    </main>
+    <Footer />
+  </div>
 </template>
+
+<script>
+export default {
+  beforeRouteUpdate(to, from, next) {
+    this.scrollToTop();
+    next();
+  },
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.scrollToTop();
+    });
+  },
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    },
+  },
+};
+</script>
