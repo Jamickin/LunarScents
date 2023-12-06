4774cc1784ed9ba6fe11d46cd5270d5b05788fb2
Jamickin <jamickina@gmail.com>
2023-12-06 12:23:51 +0200

    Star Deletion

--- LunarScents/src/App.vue
+++ LunarScents/src/App.vue
@@ -2,38 +2,36 @@
 import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
 import Footer from "./components/Footer.vue";
-import Stars from "./components/Stars.vue";
 </script>
 
 <template>
-  <div>
-    <NavBar />
-    <main
-      style="min-height: 90dvh;"
-      class="bg-gradient-to-b from-white to-Secondary sm:mt-12"
-    >
-      <RouterView />
-      <Stars />
-    </main>
-    <Footer />
-  </div>
+	<div>
+		<NavBar />
+		<main
+			style="min-height: 90dvh"
+			class="bg-gradient-to-b from-white to-Secondary sm:mt-12"
+		>
+			<RouterView />
+		</main>
+		<Footer />
+	</div>
 </template>
 
 <script>
 export default {
-  beforeRouteUpdate(to, from, next) {
-    this.scrollToTop();
-    next();
-  },
-  beforeRouteEnter(to, from, next) {
-    next(vm => {
-      vm.scrollToTop();
-    });
-  },
-  methods: {
-    scrollToTop() {
-      window.scrollTo(0, 0);
-    },
-  },
+	beforeRouteUpdate(to, from, next) {
+		this.scrollToTop();
+		next();
+	},
+	beforeRouteEnter(to, from, next) {
+		next((vm) => {
+			vm.scrollToTop();
+		});
+	},
+	methods: {
+		scrollToTop() {
+			window.scrollTo(0, 0);
+		},
+	},
 };
 </script>
